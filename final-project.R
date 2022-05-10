9a80c03bae80409843b86e2810488d72cd8c9438
Donny Robbins <robbinsdonnydr4@gmail.com>
2022-05-10 22:42:00 +0000

    added 10 more data points

--- final-project.R
+++ final-project.R
@@ -40,6 +40,18 @@ data_14 <- tidy_data("NBA14-15 data.csv")
 data_13 <- tidy_data("NBA13-14 data.csv")
 data_12 <- tidy_data("NBA12-13 data.csv")
 data_11 <- tidy_data("NBA11-12 data.csv")
+data_10 <- tidy_data("NBA10-11 data.csv")
+data_09 <- tidy_data("NBA09-10 data.csv")
+data_08 <- tidy_data("NBA08-09 data.csv")
+data_07 <- tidy_data("NBA07-08 data.csv")
+data_06 <- tidy_data("NBA06-07 data.csv")
+data_05 <- tidy_data("NBA05-06 data.csv")
+data_04 <- tidy_data("NBA04-05 data.csv")
+data_03 <- tidy_data("NBA03-04 data.csv")
+data_02 <- tidy_data("NBA02-03 data.csv")
+data_01 <- tidy_data("NBA01-02 data.csv")
+data_00 <- tidy_data("NBA00-01 data.csv")
+
 
 ten_year_data <- data_20 %>% filter(endsWith(Team,"!")) %>%
   select(everything())
@@ -51,7 +63,9 @@ find_winner <- function(data){
   return(ten_year_data)
 }
 ten_frames <- list(data_19,data_18,data_17,data_16,data_15,data_14,
-                        data_13,data_12,data_11)
+                   data_13,data_12,data_11,data_10,data_09,
+                   data_08,data_07,data_06,data_05,data_04,
+                   data_03,data_02,data_01,data_00)
 
 for (i in ten_frames){
   ten_year_data <- find_winner(i)
