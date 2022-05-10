4587a963b39c87eebf7c9a82d7cf9541aa2873f7
Donny Robbins <robbinsdonnydr4@gmail.com>
2022-05-10 00:34:35 +0000

    converted numbers to ranking values

--- final-project.R
+++ final-project.R
@@ -1,14 +1,36 @@
 library(tidyverse)
-data_all <- read_csv("basketball data.csv")
-data_all <- Filter(function(x)!all(is.na(x)), data_all) #remove empty columns
-data_p <- data_all %>% filter(endsWith(Team,"*")) %>%
-  select(everything()) %>%
+tidy_data <- function(file){
+  data <- read_csv(file)
+  data <- Filter(function(x)!all(is.na(x)), data) #remove empty columns
+  colnames(data)[18] <- "OeFG%"
+  colnames(data)[19] <- "OTOV%"
+  colnames(data)[21] <- "OFT/FGA"
+  colnames(data)[22] <- "DeFG%"
+  colnames(data)[23] <- "DTOV"
+  colnames(data)[25] <- "DFT/FGA"
+  data <- data[-c(1,26,27)]
+  
+  asc_method <- c("Age","L","PL","OTOV%","DeFG%","DFT/FGA")
+  desc_method <- c("W","PW","MOV","SOS","SRS","ORtg","DRtg","Pace","FTr",
+                   "3PAr","TS%","OeFG%","ORB%","OFT/FGA","DTOV","Attend./G",
+                   "NRtg","DRB%")
+  for (i in asc_method){
+    data[i] <- rank(data[i],ties.method = "min")
+  }
+  for (i in desc_method){
+    data[i] <- rank(-data[i],ties.method = "min")
+  }
+  return (data)
+}
+data_rk <- tidy_data("basketball data.csv")
+
+data_rk <- data_rk %>% filter(endsWith(Team,"*")) %>% #filters out nonplayoff 
+  select(everything()) %>%                            #teams
   mutate(Team = str_replace(Team,"\\*",""))
 
-data_all <- data_all %>%
-  mutate(Team = str_replace(Team,"\\*",""))
 
-print(data_all)
 
 #Rk is sorted by NRtg
 #Help from https://stackoverflow.com/questions/22104962/how-to-remove-empty-columns-in-r/27098520
+
+
